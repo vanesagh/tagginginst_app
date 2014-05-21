ceca306098b91054afe633d992319d28e89fb744
vanesagh <vanesag@chalmers.se>
2014-05-21 21:47:59 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TagginginstApp::Application.config.secret_key_base = '10bf4e3da6acff04e4e342b182e3020b70cfae9b0ca8d9b69277a2a9f78d10d6fac9e96a66fad6383922598a466d9164b0866b9597c01d7c35e471214494ecfb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TagginginstApp::Application.config.session_store :cookie_store, key: '_tagginginst_app_session'
