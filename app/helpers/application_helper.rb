70dde09426d29202d7d3d30fd04c02cb137aea2a
vanesagh <vanesag@chalmers.se>
2014-05-22 17:02:44 +0200

    add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returns the full title on  per-page basis
+  def full_title(page_title)
+    base_title = "Tagging WI"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title}: #{page_title}"
+    end
+  end
 end
