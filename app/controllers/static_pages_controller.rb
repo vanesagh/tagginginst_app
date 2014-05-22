24bf200afd7b5f789148a620352244737752d851
vanesagh <vanesag@chalmers.se>
2014-05-22 11:19:53 +0200

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def sls
+  end
+
+  def phs
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TagginginstApp::Application.routes.draw do
+  get "static_pages/sls"
+  get "static_pages/phs"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
